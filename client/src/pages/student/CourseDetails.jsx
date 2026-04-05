e2f29dba180c446546014b53e752dc157ac7c247
jamaludeen750@gmail.com <jamal@MyPC.>
2026-04-05 12:30:40 +0000

    Course Details Page : Preview (4:37:31 - 4:45:25)

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -5,6 +5,7 @@ import Loading from '../../components/student/Loading'
 import { assets } from '../../assets/assets'
 import humanizeDuration from 'humanize-duration'
 import Footer from '../../components/student/Footer'
+import Youtube from 'react-youtube'
 
 const CourseDetails = () => {
 
@@ -12,6 +13,7 @@ const CourseDetails = () => {
     const [courseData, setCourseData] = useState(null)
     const [openSections, setOpenSections] = useState({})
     const [isAlreadyEnrolled, setIsAlreadyEnrolled] = useState(true)
+    const [playerData, setPlayerData] = useState(null)
 
     const {
         allCourses,
@@ -127,7 +129,11 @@ const CourseDetails = () => {
                                                     text-gray-800 text-xs md:text-default'>
                                                         <p> {lecture.lectureTitle} </p>
                                                         <div className='flex gap-2'>
-                                                            {lecture.isPreviewFree && <p className='text-blue-500 cursor-pointer'>Preview</p>}
+                                                            {lecture.isPreviewFree && <p
+                                                                onClick={() => setPlayerData({
+                                                                    videoId: lecture.lectureUrl.split('/').pop()
+                                                                })}
+                                                                className='text-blue-500 cursor-pointer'>Preview</p>}
                                                             <p>{humanizeDuration(lecture.lectureDuration * 60 * 1000, { units: ['h', 'm'] })}</p>
 
                                                         </div>
@@ -156,7 +162,11 @@ const CourseDetails = () => {
                 {/* right column */}
                 <div className='max-w-course-card z-10 shadow-custom-card rounded-t 
                 md:rounded-none overflow-hidden bg-white max-w-md w-full'>
-                    <img src={courseData.courseThumbnail} alt='' />
+                    {
+                        playerData ? <Youtube videoId={playerData.videoId} opts={{ playerVars: { autoplay: 1 } }} iframeClassName='w-full aspect-video' />
+                            : <img src={courseData.courseThumbnail} alt='' />
+                    }
+
                     <div className='p-5'>
                         <div className="flex items-center gap-2">
                             <img src={assets.time_left_clock_icon} alt="time left clock icon" />
