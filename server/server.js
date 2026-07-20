7b488ad2ee3935f0af5962c57466bef6a39591d7
jamaludeen750@gmail.com <jamal@MyPC.>
2026-07-20 19:27:21 +0000

    added clerk logic in server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/mongodb.js";
+import { clerkWebHooks } from "./controllers/webhooks.js";
 
 //Initialize Express
 const app = express();
@@ -16,6 +17,8 @@ app.use(cors());
 //Routes
 app.get("/", (req, res) => res.send("API Working"));
 
+
+app.post('/clerk', express.json(), clerkWebHooks)
 //PORT
 
 const PORT = process.env.PORT || 5000
