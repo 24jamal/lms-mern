f31cdd60bb53e22ac3b51e3b0301b24179675f96
jamaludeen750@gmail.com <jamal@MyPC.>
2026-07-21 13:59:52 +0000

    Added Schemas of Course, Chapter, Lecture and Cloudinary Codes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/mongodb.js";
 import { clerkWebHooks } from "./controllers/webhooks.js";
+import educatorRouter from "./routes/educatorRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
+import connectCloudinary from "./configs/cloudinary.js";
 
 //Initialize Express
 const app = express();
@@ -10,9 +13,11 @@ const app = express();
 
 //Connect to database
 await connectDB()
+await connectCloudinary()
 
 //Middlewares
 app.use(cors());
+app.use(clerkMiddleware());
 
 //Routes
 app.get("/", (req, res) => res.send("API Working"));
@@ -20,6 +25,7 @@ app.get("/", (req, res) => res.send("API Working"));
 
 app.post('/clerk', express.json(), clerkWebHooks)
 //PORT
+app.use('/api/educator', express.json(), educatorRouter)
 
 const PORT = process.env.PORT || 5000
 
