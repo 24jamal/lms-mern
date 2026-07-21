a896e2c66e4e49ebba92f5a24c0b0017a4e38e02
jamaludeen750@gmail.com <jamal@MyPC.>
2026-07-21 05:42:04 +0000

    Rectified webhooks.js bugs

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -22,7 +22,7 @@ export const clerkWebHooks = async (req, res) => {
                     _id: data.id,
                     email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
-                    imageUrl: data.imageUrl,
+                    imageUrl: data.image_url,
 
                 }
 
@@ -35,13 +35,16 @@ export const clerkWebHooks = async (req, res) => {
                 const userData = {
                     email: data.email_address[0].email_address,
                     name: data.first_name + " " + data.last_name,
-                    imageUrl: data.imageUrl,
+                    imageUrl: data.image_url,
                 }
                 await User.findByIdAndUpdate(data.id, userData)
+                res.json({})
+                break;
             }
 
             case 'user.deleted': {
-                await User.findByIdAndDelete(data)
+                await User.findByIdAndDelete(data.id)
+                res.json({})
                 break;
             }
 
